9d58ada0864324d2196aa0db3985545985f3a54b
Arman <alotf@florencenext.com>
2026-04-23 17:25:00 +0200

    fix: adds breakpoints

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={museoSans.variable}>
-      <body className="min-h-screen flex flex-col">
+      <body className="mx-auto max-w-[1920px] bg-white min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
